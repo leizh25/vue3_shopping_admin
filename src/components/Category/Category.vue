cf92d8fc4c4078687dec975766055d57d6377fb7
leizh25 <leizhuzheng@qq.com>
2023-10-14 22:14:10 +0800

    添加与修改属性的静态搭建

--- src/components/Category/Category.vue
+++ src/components/Category/Category.vue
@@ -2,18 +2,18 @@
   <el-card>
     <el-form :inline="true">
       <el-form-item label="一级分类">
-        <el-select v-model="categoryStore.c1Id" @change="handler1">
+        <el-select v-model="categoryStore.c1Id" @change="handler1" :disabled="scene == 1">
           <!-- label即为展示的数据,展示的数据不可以用插值语法写在标签内,否则change事件触发时,标签内显示的就是ID  value即为select下拉菜单收集的数据 -->
           <el-option v-for="c1 in categoryStore.c1Arr" :key="c1.id" :value="c1.id" :label="c1.name"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类">
-        <el-select v-model="categoryStore.c2Id" @change="handler2">
+        <el-select v-model="categoryStore.c2Id" @change="handler2" :disabled="scene == 1">
           <el-option v-for="c2 in categoryStore.c2Arr" :key="c2.id" :value="c2.id" :label="c2.name"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="三级分类" v-model="categoryStore.c3Id">
-        <el-select v-model="categoryStore.c3Id">
+        <el-select v-model="categoryStore.c3Id" :disabled="scene == 1">
           <el-option v-for="c3 in categoryStore.c3Arr" :key="c3.id" :value="c3.id" :label="c3.name"></el-option>
         </el-select>
       </el-form-item>
@@ -53,5 +53,8 @@ const handler2 = () => {
   categoryStore.c3Arr = []
   categoryStore.getC3()
 }
+
+//接收父组件传过来的场景值
+defineProps(['scene'])
 </script>
 <style scoped></style>
--- src/views/Product/Attr/Attr.vue
+++ src/views/Product/Attr/Attr.vue
@@ -1,25 +1,44 @@
 <template>
   <div>
     <!-- 三级分类全局组件 -->
-    <Category></Category>
+    <Category :scene="scene"></Category>
     <el-card style="margin: 10px 0">
-      <el-button type="primary" icon="plus" :disabled="!categoryStore.c3Id">添加属性</el-button>
-      <el-table border style="margin: 10px 0" :data="attrArr">
-        <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
-        <el-table-column label="属性名称" width="120px" prop="attrName"></el-table-column>
-        <el-table-column label="属性值名称">
-          <template #default="{ row }">
-            <el-tag style="margin: 5px" v-for="item in row.attrValueList" :key="item.id">{{ item.valueName }}</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column label="操作" width="120px">
-          <template #default>
-            <!-- row:已有的属性对象 -->
-            <el-button type="primary" size="small" icon="edit"></el-button>
-            <el-button type="danger" size="small" icon="delete"></el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+      <div v-show="scene == 0">
+        <el-button type="primary" icon="plus" :disabled="!categoryStore.c3Id" @click="addAttr">添加属性</el-button>
+        <el-table border style="margin: 10px 0" :data="attrArr">
+          <el-table-column label="序号" type="index" align="center" width="80px"></el-table-column>
+          <el-table-column label="属性名称" width="120px" prop="attrName"></el-table-column>
+          <el-table-column label="属性值名称">
+            <template #default="{ row }">
+              <el-tag style="margin: 5px" v-for="item in row.attrValueList" :key="item.id">{{ item.valueName }}</el-tag>
+            </template>
+          </el-table-column>
+          <el-table-column label="操作" width="120px">
+            <template #default>
+              <!-- row:已有的属性对象 -->
+              <el-button type="primary" size="small" icon="edit" @click="updateAttr"></el-button>
+              <el-button type="danger" size="small" icon="delete"></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+      <div v-show="scene == 1">
+        <!-- 展示添加与修改属性的结构 -->
+        <el-form :inline="true">
+          <el-form-item label="属性名称">
+            <el-input placeholder="请你输入属性的名称"></el-input>
+          </el-form-item>
+        </el-form>
+        <el-button type="primary" icon="plus">添加属性值</el-button>
+        <el-button type="primary" @click="cancel">取消</el-button>
+        <el-table border style="margin: 10px 0">
+          <el-table-column width="80" type="index" label="序号" align="center"></el-table-column>
+          <el-table-column label="属性值名称"></el-table-column>
+          <el-table-column label="属性值操作"></el-table-column>
+        </el-table>
+        <el-button type="primary">保存</el-button>
+        <el-button type="primary" @click="cancel">取消</el-button>
+      </div>
     </el-card>
   </div>
 </template>
@@ -36,6 +55,8 @@ import type { AttrResponseData, Attr } from '@/api/product/attr/type'
 const categoryStore = useCategoryStore()
 //存储已有的属性与属性值
 let attrArr = ref<Attr[]>([])
+//定义卡片组件内容切换的变量
+let scene = ref<number>(1) //scene=0显示table, scene=1,展示添加与修改属性结构
 //监听仓库三级分类ID变化
 watch(
   () => categoryStore.c3Id,
@@ -57,6 +78,20 @@ const getAttr = async () => {
   if (res.code == 200) {
     attrArr.value = res.data
   }
+}
+//添加属性按钮的回调
+const addAttr = () => {
+  //切换到添加与修改属性的机构
+  scene.value = 1
+}
+//table表格中修改已有属性按钮的回调
+const updateAttr = () => {
+  scene.value = 1
+}
+
+//取消按钮的回调
+const cancel = () => {
+  scene.value = 0
 }
 </script>
 <style scoped></style>
