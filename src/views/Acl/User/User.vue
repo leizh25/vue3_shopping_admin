f1256221cd9848d58eced41157548d15c091f563
leizh25 <leizhuzheng@qq.com>
2023-11-13 15:31:32 +0800

    添加与修改用户的静态搭建

--- src/views/Acl/User/User.vue
+++ src/views/Acl/User/User.vue
@@ -12,7 +12,7 @@
       </el-form>
     </el-card>
     <el-card>
-      <el-button type="primary">添加用户</el-button>
+      <el-button type="primary" @click="addUser">添加用户</el-button>
       <el-button type="danger">批量删除</el-button>
       <!-- 表格展示用户信息 -->
       <el-table style="margin: 10px 0" border :data="userArr">
@@ -27,7 +27,7 @@
         <el-table-column label="操作" width="270px" align="center">
           <template #default="{ row }">
             <el-button type="primary" size="small" icon="user">分配角色</el-button>
-            <el-button type="primary" size="small" icon="edit">编辑</el-button>
+            <el-button type="primary" size="small" icon="edit" @click="updateUser(row)">编辑</el-button>
             <el-button type="danger" size="small" icon="delete">删除</el-button>
           </template>
         </el-table-column>
@@ -44,12 +44,38 @@
         @size-change="sizeChangeHandle"
       ></el-pagination>
     </el-card>
+    <!-- 抽屉结构:完成添加新的用户账号|更新已有的账号信息 -->
+    <el-drawer v-model="isShowDrawer" direction="rtl">
+      <!-- 头部标题:文字标题应该是动态的 -->
+      <template #header>
+        <h4>添加用户</h4>
+      </template>
+      <template #default>
+        <el-form>
+          <el-form-item label="用户姓名:">
+            <el-input placeholder="请输入姓名"></el-input>
+          </el-form-item>
+          <el-form-item label="用户昵称:">
+            <el-input placeholder="请输入昵称"></el-input>
+          </el-form-item>
+          <el-form-item label="用户密码:">
+            <el-input placeholder="请输入密码"></el-input>
+          </el-form-item>
+        </el-form>
+      </template>
+      <template #footer>
+        <div style="flex: auto">
+          <el-button>取消</el-button>
+          <el-button type="primary">确定</el-button>
+        </div>
+      </template>
+    </el-drawer>
   </div>
 </template>
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { reqUserList } from '@/api/acl/user/index.ts'
-import { UserResponseData, Records } from '@/api/acl/user/type'
+import { UserResponseData, Records, User } from '@/api/acl/user/type'
 //默认第一页
 let pageNo = ref<number>(1)
 //一页展示几条数据
@@ -58,13 +84,15 @@ let pageSize = ref<number>(5)
 let total = ref<number>(0)
 //存储全部用户的数组
 let userArr = ref<Records>([])
+//定义响应式数据控制抽屉的显示与隐藏
+let isShowDrawer = ref<boolean>(false)
 
 //获取全部已有的账号信息
 const getHasUsers = async (page = 1) => {
   //收集当前页码
   pageNo.value = page
   const res: UserResponseData = await reqUserList(pageNo.value, pageSize.value)
-  console.log('res: ', res)
+  // console.log('res: ', res)
   //存储数据
   if (res.code === 200) {
     total.value = res.data.total
@@ -78,6 +106,16 @@ onMounted(() => {
 //分页器下拉菜单自定义事件回调
 const sizeChangeHandle = () => {
   getHasUsers()
+}
+//添加用户按钮回调
+const addUser = () => {
+  //显示抽屉
+  isShowDrawer.value = true
+}
+//更新用户按钮回调
+const updateUser = (user: User) => {
+  //显示抽屉
+  isShowDrawer.value = true
 }
 </script>
 <style scoped>
