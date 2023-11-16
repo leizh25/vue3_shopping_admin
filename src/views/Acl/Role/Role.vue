683228079950f31519bf64ee4851bc824827e831
leizh25 <leizhuzheng@qq.com>
2023-11-16 23:27:25 +0800

    添加与更新角色业务

--- src/views/Acl/Role/Role.vue
+++ src/views/Acl/Role/Role.vue
@@ -40,25 +40,26 @@
       />
     </el-card>
     <!-- 添加职位与更新职位的对话框弹窗 -->
-    <el-dialog v-model="isSHowDialog" title="添加职位">
-      <el-form>
-        <el-form-item label="职位名称">
-          <el-input placeholder="输入职位名称"></el-input>
+    <el-dialog v-model="isSHowDialog" :title="`${RoleParams.id ? '更新' : '添加'}角色`">
+      <el-form :model="RoleParams" :rules="rules" ref="roleNameFormRef">
+        <el-form-item label="职位名称" prop="roleName">
+          <el-input placeholder="输入职位名称" v-model="RoleParams.roleName"></el-input>
         </el-form-item>
       </el-form>
       <template #footer>
         <el-button @click="isSHowDialog = false">取消</el-button>
-        <el-button type="primary">确定</el-button>
+        <el-button type="primary" @click="save">确定</el-button>
       </template>
     </el-dialog>
   </div>
 </template>
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
-import { reqAllRoleList } from '@/api/acl/role'
+import { nextTick, onMounted, reactive, ref } from 'vue'
+import { reqAllRoleList, reqAddOrUpdateRole } from '@/api/acl/role'
 import { Records, RoleResponseData } from '@/api/acl/role/type'
 import useLayoutSettingStore from '@/store/modules/setting'
-import { RoleData } from '@/api/acl/user/type'
+import { RoleData } from '@/api/acl/role/type'
+import { ElMessage } from 'element-plus'
 const layoutSettingStore = useLayoutSettingStore()
 //当前页码
 const pageNo = ref<number>(1)
@@ -72,6 +73,12 @@ let allRole = ref<Records[]>([])
 let total = ref<number>(0)
 //对话框的显示与隐藏
 let isSHowDialog = ref<boolean>(false)
+//收集新增岗位的数据
+let RoleParams = reactive<RoleData>({
+  roleName: '',
+})
+//职位表单组件
+const roleNameFormRef = ref<any>()
 //组件挂载完毕
 onMounted(() => {
   // 加载数据
@@ -102,10 +109,49 @@ const reset = () => {
 //添加职位按钮回调
 const addRole = () => {
   isSHowDialog.value = true
+  //清空表单数据
+  Object.assign(RoleParams, {
+    roleName: '',
+    id: 0,
+  })
+  //清空上一次表单校验的结果
+  nextTick(() => roleNameFormRef.value.clearValidate())
 }
 //更新职位按钮回调
 const updateRole = (role: RoleData) => {
   isSHowDialog.value = true
+  // console.log('role: ', role)
+  //存储职位 -- 带有ID
+  Object.assign(RoleParams, role)
+  //清空上一次校验的结果
+  nextTick(() => roleNameFormRef.value.clearValidate())
+}
+//自定义校验规则
+const validateRoleName = (_rule: any, value: string, callback: any) => {
+  if (value.trim().length < 2) {
+    callback(new Error('职位名称至少两位字符'))
+  } else {
+    callback()
+  }
+}
+//职位表单校验规则
+const rules = {
+  roleName: [{ required: true, trigger: 'blur', validator: validateRoleName }],
+}
+//职位表单确定按钮的回调
+const save = async () => {
+  //表单校验,结果通过发请求,未通过不该发请求
+  await roleNameFormRef.value.validate()
+  //添加/更新职位的请求
+  const res = await reqAddOrUpdateRole(RoleParams)
+  // console.log('res: ', res)
+  if (res.code === 200) {
+    ElMessage.success(`${RoleParams.id ? '更新' : '添加'}职位成功`)
+    isSHowDialog.value = false
+    getHasRole(RoleParams.id ? pageNo.value : 1)
+  } else {
+    ElMessage.error(res.message)
+  }
 }
 </script>
 <style scoped>
