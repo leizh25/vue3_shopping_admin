ff587acd234e7bec417d309b2d0c17efe04d077d
leizh25 <leizhuzheng@qq.com>
2024-02-28 17:48:29 +0800

    水球图完成

--- src/views/Screen/components/tourist.vue
+++ src/views/Screen/components/tourist.vue
@@ -13,12 +13,46 @@
       <span v-for="item in people" :key="item" class="people">{{ item }}</span>
     </div>
     <!-- 盒子将来echarts展示图形图表节点 -->
-    <div class="charts">123</div>
+    <div class="charts" ref="charts">123</div>
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
 let people = ref('216908人')
+import * as echarts from 'echarts'
+//水球图扩展插件
+import 'echarts-liquidfill'
+//获取节点
+let charts = ref()
+onMounted(() => {
+  //获取echarts类的实例
+  let mycharts = echarts.init(charts.value)
+  //设置实例配置对象
+  mycharts.setOption({
+    // 标题组件
+    title: {
+      text: '水球图',
+    },
+    // x|y轴组件
+    xAxis: {},
+    yAxis: {},
+    series: {
+      type: 'liquidFill', //系列
+      data: [0.6, 0.4, 0.2], //展示的数据
+      waveAnimation: true, //动画
+      animationDuration: 3, //动画时间
+      animationDurationUpdate: 0, //
+      radius: '100%', //半径
+    },
+    //布局组件
+    grid: {
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    },
+  })
+})
 </script>
 <style lang="scss" scoped>
 .box {
@@ -68,6 +102,7 @@ let people = ref('216908人')
       color: #29fcff;
     }
   }
+
   .charts {
     width: 100%;
     height: 250px;
